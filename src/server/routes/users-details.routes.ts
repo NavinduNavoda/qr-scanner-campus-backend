d138d6afcada5c66a0a4251a072b0d124ea5d602
Hansa Gunasinghe <wahansaliviru@gmail.com>
2024-10-25 08:02:57 +0530

    uu

--- src/server/routes/users-details.routes.ts
+++ src/server/routes/users-details.routes.ts
@@ -87,6 +87,7 @@ UserDetailsRouter.post('/single-demo', async (req, res) => {
 
 UserDetailsRouter.post('/single-user', async (req, res) => {
     try {
+        console.log("[+] single user details req.body: ", req.body);
         const { id } = req.body;
         const stmt = sqliteDB.prepare("SELECT id, name, email, phone, sc_number, role, isActive, created_at FROM user WHERE id = ?");
         const user = stmt.get(id);
